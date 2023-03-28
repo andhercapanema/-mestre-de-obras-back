9cf70024f6274f5b423ae5c0c99b439450c347e9
andherpaulo <andher.dev@gmail.com>
2023-03-28 12:08:32 -0300

    feat: add name column in user model | refact: translate errors messages

--- src/services/users-service/errors.ts
+++ src/services/users-service/errors.ts
@@ -3,6 +3,6 @@ import { ApplicationError } from "@/errors/protocols";
 export function duplicatedEmailError(): ApplicationError {
     return {
         name: "DuplicatedEmailError",
-        message: "There is already an user with given email",
+        message: "Já existe um usuário cadastrado com esse e-mail",
     };
 }
